f34adea622b2e10dbc90c204245d84beceef2255
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-08 14:05:27 +0800

    Update setting.py
    改變顏色增加終端打印的舒適度

--- src/setting.py
+++ src/setting.py
@@ -12,11 +12,13 @@ logging.basicConfig(
         fmt=logging.BASIC_FORMAT, file_path='./logs\\log.txt', debug_file_path='./logs\\DEBUG_log.txt')],
 )
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+setting_file = os.path.join('../config/setting.json')
+
 
 def check_chrome_notify_log():
 
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    setting_file = os.path.join('../config/setting.json')
+    global setting_file
 
     with open(str(setting_file), 'r') as json_file:
         setting_chrome_notify = json.load(json_file)
@@ -28,9 +30,11 @@ def check_chrome_notify_log():
     if platform in chrome_notify:
         path = chrome_notify[platform]
         if os.path.exists(path):
-            print('Successfully - Chrome notifications log file found.')
-            print("\033[34mNotification of Continuous checking... \033[0m")
             logging.info(f"File found: {path}")
+            print(
+                '\033[38;2;255;255;153mSuccessfully\033[m - \033[38;2;204;245;255mChrome notifications log file found!\033[m')
+            print(
+                '\033[38;2;153;235;255mNotification of Continuous checking...\033[m')
             return True, path
         else:
             (logging.error(f"File not found: {path}"))
