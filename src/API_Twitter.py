8a9cf08391cb52815c34162678af6e9a206f208f
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-13 22:29:32 +0800

    Update API_Twitter.py Development 捨棄 Main.py

--- src/API_Twitter.py
+++ src/API_Twitter.py
@@ -3,12 +3,12 @@ import logging
 import os
 import pickle
 
-from src.custom_log import ColoredLogHandler
-from src.twitter.API_match_Twitter_account import (
+from custom_log import ColoredLogHandler
+from twitter.API_match_Twitter_account import (
     TwitterAccountProcessor,
     Error_Log_Handler,
 )
-from src.twitter.Twitter_rss_process import *
+from twitter.Twitter_rss_process import *
 
 # TODO:未來可能寫 TOML 或 Table文件來寫白名單
 
@@ -19,29 +19,33 @@ re_Tweet_switch = True
 class API_Twitter:
 
     def __init__(self):
+
         current_dir = os.getcwd()
+        parent_dir = os.path.dirname(current_dir)
+
         self.Twitter_cache_list = []
         self.Twitter_cache_dict_pkl = os.path.join(
-            current_dir, 'assets', 'Twitter_cache_dict.pkl'
+            parent_dir, "assets", "Twitter_cache_dict.pkl"
         )
         self.Twitter_dict_json = os.path.join(
-            current_dir, 'assets', 'Twitter_dict.json'
+            parent_dir, "assets", "Twitter_dict.json"
         )
 
         logging.basicConfig(
             level=logging.INFO,
             handlers=[
                 ColoredLogHandler(
-                    fmt=logging.BASIC_FORMAT,
-                )
-            ],
+                    fmt=logging.BASIC_FORMAT)
+            ]
         )
 
     def match_twitter_account(self, input_notify: str):
 
+        re_Tweet_check = None
+
         re_Tweet_check = TwitterEntry_re_Tweet.entry_url_process(
             input_notify
-        )  # 得到匹配到的轉推來源帳號或None等於非轉推
+        )  # 得到匹配到的轉推 str=來源帳號 或 None=非轉推
 
         # match is Twitter account or not
         match_output_tuple = TwitterAccountProcessor(
@@ -54,41 +58,51 @@ class API_Twitter:
         
         tuple(binary_search_result -> True, return value || twitter_link(Hardcode), twitter_post_id)
         """
+
+        print('\033[95mthis is match output and re_Tweet_cheek:\033[0m')
+        print(type(match_output_tuple), type(re_Tweet_check))
+        print(match_output_tuple, re_Tweet_check)
+
         if match_output_tuple is None:
-            Error_Log_Handler.error_log()
-            raise ValueError(False)
+            re_Tweet_check = None
+            match_output_tuple = None
+            pass
         else:
+            print('\033[92mAPI_Twitter 開始運作!\033[0m')
             return tuple(match_output_tuple), re_Tweet_check
 
-    
     def process_twitter_account(self, input_notify):
-        
+
         result_tuple = self.match_twitter_account(input_notify)
-        
+
         if result_tuple is None:
+            re_Tweet_check = None
+            match_output_tuple = None
             Error_Log_Handler.error_log()
         else:
-            match_output_tuple, re_Tweet_check = self.match_twitter_account(input_notify)
+            match_output_tuple, re_Tweet_check = self.match_twitter_account(
+                input_notify)
 
-        if re_Tweet_check == match_output_tuple[1] and re_Tweet_switch == True:
-            print(
-                "\033[91mWarning: \033[33m\033[38;2;255;255;179m轉推阻擋已開啟，本次請求 \033[91m已阻擋 \033[33m"
-                "\033[38;2;255;255;179m若要關閉\033[38;2;255;255;179m此功能在\033[0m"
-                "\033[36m API_Twitter.py\033[91m \033[38;2;255;255;179m"
-                "\033[0m將 \033[36mre_Tweet_switch\033[0m 變數設為\033[33m \033[36mFalse\033[0m"
-            )
-        elif match_output_tuple is None:
-            pass
-        # match_output_tuple[0] is binary search bool
-        elif bool(match_output_tuple[0]) is False:
-            pass
-        elif not match_output_tuple is False:
-            self.process_valid_tweet(match_output_tuple)
-    
+            # 轉推判斷
+            if re_Tweet_check == match_output_tuple[1] and re_Tweet_switch == True:
+                print(
+                    "\033[91mWarning: \033[33m\033[38;2;255;255;179m轉推阻擋已開啟，本次請求 \033[91m已阻擋 \033[33m"
+                    "\033[38;2;255;255;179m若要關閉\033[38;2;255;255;179m此功能在\033[0m"
+                    "\033[36m API_Twitter.py\033[91m \033[38;2;255;255;179m"
+                    "\033[0m將 \033[36mre_Tweet_switch\033[0m 變數設為\033[33m \033[36mFalse\033[0m"
+                )
+            else:
+                # match_output_tuple[0] is binary search bool
+                if bool(match_output_tuple[0]) is False:
+                    Error_Log_Handler.error_log()
+                elif not match_output_tuple is False:
+                    self.process_valid_tweet(match_output_tuple)
+
     def process_valid_tweet(self, match_output_tuple):
 
         # 網址哈希一下再存進去
-        MD5 = hashlib.md5(str(match_output_tuple[2]).encode("utf-8")).hexdigest()
+        MD5 = hashlib.md5(
+            str(match_output_tuple[2]).encode("utf-8")).hexdigest()
 
         # 把 match Tag 轉成 str 後去除全部 ['()'] 字元
         Twitter_id = (
@@ -104,24 +118,26 @@ class API_Twitter:
         # list(Twitter_cache_list)  ['qcpk0203','c682e42712551f88dfcefe076e2aeb93']
         self.Twitter_cache_list.append(Twitter_id)
         self.Twitter_cache_list.append(MD5)
-        
+
+        print('帳號:', Twitter_id)
+
         is_in_Dict = Key_Exists_in_Dict().dict_key_found(MD5)
 
         if int(is_in_Dict) == 1:
-            logging.info("此次通知已經存在資料庫中，不再發起後續請求")
+            logging.info("此通知已經存在資料庫中，不再新增")
         elif int(is_in_Dict) == 0:
             self.__save_to_pkl_()  # 以List存入['Twitter_id, MD5']
 
             Twitter().start_request(Twitter_id)  # 呼叫HTTP RSS請求和存入字典的操作
             TwitterMatcher().start_match()  # 查找HashTag標籤並匹配IVE成員是誰或不只匹配到一人
-            
+
     # 私有方法
     def __save_to_pkl_(self):
 
         # 開始存入 value 到 ../assets/Twitter_cache_dict.pkl
         try:
-            with open(self.Twitter_cache_dict_pkl, "rb") as pkl:
-                existing_data = pickle.load(pkl)
+            with open(self.Twitter_cache_dict_pkl, "rb") as file:
+                existing_data = pickle.load(file)
         except FileNotFoundError:
             with open(self.Twitter_cache_dict_pkl, "wb") as pkl:
                 pickle.dump(list(self.Twitter_cache_list), pkl)
@@ -132,11 +148,11 @@ class API_Twitter:
 
         # 將新的數據追加到現有數據中
         existing_data.append(self.Twitter_cache_list)
-        
+
         # [['qcpk0203', 'c682e42712551f88dfcefe076e2aeb93']]
         # 將更新後的數據寫入文件
-        with open(self.Twitter_cache_dict_pkl, "wb") as pkl:
-            pickle.dump(existing_data, pkl)
+        with open(self.Twitter_cache_dict_pkl, "wb") as file:
+            pickle.dump(existing_data, file)
 
 
 class TwitterEntry_re_Tweet:
@@ -144,9 +160,11 @@ class TwitterEntry_re_Tweet:
     @staticmethod
     def entry_url_process(input_data):
         # 匹配 Twitter 轉推條目的 URL
-        tw_regex = re.findall(r"https://twitter.com/.*?tweet\b", input_data, re.DOTALL)
+        tw_regex = re.findall(
+            r"https://twitter.com/.*?tweet\b", input_data, re.DOTALL)
         # 匹配轉推條目的 URL
-        tw_regex_url = re.findall(r'https://twitter.com/(?:#1tweet|")', input_data)
+        tw_regex_url = re.findall(
+            r'https://twitter.com/(?:#1tweet|")', input_data)
 
         match_accounts = None
         try:
@@ -172,22 +190,25 @@ class TwitterEntry_re_Tweet:
 
                     # 將匹配到的 Twitter 帳號組合為一個字符串，如果未匹配到則返回 None
                     match_accounts = (
-                        " ".join([_[0] for _ in matches_data]) if matches_data else None
+                        " ".join([_[0] for _ in matches_data]
+                                 ) if matches_data else None
                     )
         except IndexError:
             match_accounts = None
         # 返回匹配到的轉推來源帳號或者返回 None
         return match_accounts
 
+
 class Key_Exists_in_Dict:
 
     def read_twitter_dict(self):
 
         current_dir = os.getcwd()
-        file_path = os.path.join(current_dir, '../../assets',  'Twitter_dict.json')
-        
-        print('Debug from API_Twitter: 189Line file path check', file_path)
-        
+        file_path = os.path.join(current_dir, "../assets/Twitter_dict.json")
+
+        print("\n\033[9;33;40m路徑追蹤\033[0m" +
+              'class Key_Exists_in_Dict ', file_path, '-' * 50)
+
         if os.path.isfile(file_path):
             with open(file_path, "r") as f:
                 twitter_dict = json.load(f)
