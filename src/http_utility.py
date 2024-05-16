190f5cdf82ba5dd2e299ab5a748237338469f148
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-16 10:41:47 +0800

    Update http_utility.py aiohttp

--- src/http_utility.py
+++ src/http_utility.py
@@ -1,7 +1,5 @@
+import aiohttp
 import logging
-import random
-import time
-import requests
 from datetime import datetime
 
 from fake_useragent import UserAgent
@@ -17,44 +15,25 @@ class HttpRequester:
         self.url = url
         self.response_content = None
 
-    def send_request(self):
-        response = requests.get(self.url, timeout=10)
-        self.response_content = response.text
-
-    def get_response_content(self):
-        return str(self.response_content)
-
-    def start_requests(self):
+    async def send_request(self):
         ua = UserAgent()
         headers = {"user-agent": ua.random}
-        try:
-            response = requests.get(self.url, headers=headers, timeout=10)
-            if response.status_code == 200:
-                print(
-                    f"\033[38;2;102;153;153mSystem current time: {SystemTime.format_current_time()}\033[0m")
-                self.response_content = response.text
-                logging.info(
-                    f" {self.url} code: {response.status_code}")
-                return response.status_code
-            else:
-                print(
-                    f"\033[38;2;102;153;153mSystem current time: {SystemTime.format_current_time()}\033[0m")
-                self.response_content = response.text
-                logging.warning(
-                    f" {self.url} code: {response.status_code}")
-                return None
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url, headers=headers, timeout=10) as response:
+                self.response_content = await response.text()
+                return response.status
 
-        except TimeoutError as e:
+    async def get_response_content(self):
+        return str(self.response_content)
+
+    async def start_requests(self):
+        status_code = await self.send_request()
+        current_time = SystemTime.format_current_time()
+        if status_code == 200:
             print(
-                f"\033[38;2;102;153;153mSystem current time: {SystemTime.format_current_time()}\033[0m")
-            logging.error(
-                f"{self.url} Timeout Error: {str(e)}"
-            )
-            return None
-        except Exception as e:
+                f"\033[38;2;102;153;153mSystem current time: {current_time}\033[0m")
+            logging.info(f" {self.url} code: {status_code}")
+        else:
             print(
-                f"\033[38;2;102;153;153mSystem current time: {SystemTime.format_current_time()}\033[0m")
-            logging.error(
-                f"{self.url} Unexpected error: {str(e)}"
-            )
-            return None
+                f"\033[38;2;102;153;153mSystem current time: {current_time}\033[0m")
+            logging.warning(f" {self.url} code: {status_code}")
