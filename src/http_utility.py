e880680f03d9fc54522fadbef22d2dd7ee2ca1fc
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-16 11:24:35 +0800

    Update http_utility.py 錯誤捕捉更新 使用者體驗更新

--- src/http_utility.py
+++ src/http_utility.py
@@ -14,26 +14,38 @@ class HttpRequester:
     def __init__(self, url):
         self.url = url
         self.response_content = None
+        self.status_message = "失敗，無法連線"
 
     async def send_request(self):
         ua = UserAgent()
         headers = {"user-agent": ua.random}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.url, headers=headers, timeout=10) as response:
-                self.response_content = await response.text()
-                return response.status
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url, headers=headers, timeout=20) as response:
+                    self.response_content = await response.text()
+                    self.status_message = f"HTTP Status Code: {response.status}"
+                    return response.status
+        except aiohttp.client_exceptions.ClientOSError as e:
+            logging.error(f"ClientOSError: {e} - 指定的網路名稱無法使用")
+            return "失敗，無法連線"
+        except TimeoutError as e:
+            logging.error(f"TimeoutError: {e} - 指定的網路名稱無法連線")
+            return "失敗，Timed out"
 
     async def get_response_content(self):
         return str(self.response_content)
 
     async def start_requests(self):
-        status_code = await self.send_request()
+        status_code_or_message = await self.send_request()
         current_time = SystemTime.format_current_time()
-        if status_code == 200:
+        if status_code_or_message == 200:
             print(
                 f"\033[38;2;102;153;153mSystem current time: {current_time}\033[0m")
-            logging.info(f" {self.url} code: {status_code}")
+            logging.info(f" {self.url} code: {status_code_or_message}")
         else:
             print(
                 f"\033[38;2;102;153;153mSystem current time: {current_time}\033[0m")
-            logging.warning(f" {self.url} code: {status_code}")
+            if isinstance(status_code_or_message, int):
+                logging.warning(f" {self.url} code: {status_code_or_message}")
+            else:
+                logging.warning(f" {self.url} - {status_code_or_message}")
