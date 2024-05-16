40998d40d1cdbfcdde637880c11d2ec0741e0952
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-16 20:01:33 +0800

    Update timezone.py

--- src/timezone.py
+++ src/timezone.py
@@ -1,46 +1,23 @@
 from datetime import datetime
-import pytz
+from zoneinfo import ZoneInfo
+
+from aiocache import cached
 
 
 class TimeZoneConverter:
 
-    def __init__(self, from_tz, to_tz):
-        # 初始化方法，設定資訊來源的時區'from_tz'和目的地時區'to_tz'，並將這兩個時區物件保留在物件的屬性中
-        self.from_tz = pytz.timezone(from_tz)
-        self.to_tz = pytz.timezone(to_tz)
-        pass
+    from_tz = ZoneInfo('GMT')
+    to_tz = ZoneInfo('Asia/Taipei')
 
-    def convert_time_gmt_to_utc(
-        self,
-        time_str,
-        format="%a, %d %b %Y %H:%M:%S %Z",
-        output_format="%Y/%m/%d %H:%M:%S",
-    ):
-        # 將來源時區的時間字串轉換為目的地時區的時間字串
-        time_obj = datetime.strptime(time_str, format)  # 將時間字串解析為datetime物件
-        time_obj_from_tz = self.from_tz.localize(time_obj)  # 為時間物件增加來源時區資訊
-        time_obj_to_tz = time_obj_from_tz.astimezone(
-            self.to_tz
-        )  # 將時間物件轉換為目的地時區
-        return time_obj_to_tz.strftime(
-            output_format
-        )  # 將時間物件格式化為字串，並返回轉換後的時間字串
-
-    def format_time_slice(
-        self,
-        time_str,
-        format="%a, %d %b %Y %H:%M:%S %Z",
-        output_format="%Y%m%d %H:%M:%S",
-    ):
-        # 用來將給定的GMT時間字串轉換為在UTC+8時區的特定格式時間字串
-        return self.convert_time_gmt_to_utc(
-            time_str, format, output_format
-        )  # 呼叫時區轉換器進行轉換
+    @cached(ttl=3600)
+    async def convert_time(self, time_str: str, format: str = "%a, %d %b %Y %H:%M:%S %Z", output_format: str = "%Y/%m/%d %H:%M:%S") -> str:
+        try:
+            time_obj = datetime.strptime(
+                time_str, format).replace(tzinfo=self.from_tz)
+            return time_obj.astimezone(self.to_tz).strftime(output_format)
+        except ValueError as e:
+            raise ValueError(f"時間格式錯誤: {e}")
 
 
-class SystemTime:
-
-    @ staticmethod
-    def format_current_time(format_string="%Y/%m/%d %H:%M:%S"):
-        time_current = datetime.now()
-        return time_current.strftime(format_string)
+async def get_formatted_current_time(format_string: str = "%Y/%m/%d %H:%M:%S") -> str:
+    return datetime.now().strftime(format_string)
