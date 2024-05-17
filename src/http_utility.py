ad735965f22f5da9dd5a7448900c5c51974a63a8
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-17 12:06:45 +0800

    Update http_utility.py

--- src/http_utility.py
+++ src/http_utility.py
@@ -13,15 +13,15 @@ class HttpRequester:
     def __init__(self, url):
         self.url = url
         self.response_content = None
-        self.status_message = "失敗，無法連線"
-        self.headers = {"user-agent": UserAgent().random}
-        self.session = aiohttp.ClientSession()
+        self.status_message = "失敗，無法連線"  # 默認狀態消息為“失敗，無法連接”
+        self.headers = {"user-agent": UserAgent().random}  # 隨機生成User-Agent
+        self.session = aiohttp.ClientSession()  # 創建Aiohttp客戶端會話
 
     async def send_request(self):
-        timeout = ClientTimeout(total=20)
+        timeout = ClientTimeout(total=20)  # 設置超時時間為20秒
         try:
             async with self.session.get(self.url, headers=self.headers, timeout=timeout) as response:
-                self.response_content = await response.text()
+                self.response_content = await response.text()  # 獲取響應內容
                 self.status_message = f"HTTP Status Code: {response.status}"
                 return response.status
         except aiohttp.ClientError as e:
@@ -29,14 +29,15 @@ class HttpRequester:
             return "失敗，網路請求錯誤"
 
     async def get_response_content(self):
+        # 獲取響應內容並轉換為字符串
         return str(self.response_content)
 
     async def start_requests(self):
-        status_code_or_message = await self.send_request()
+        status_code_or_message = await self.send_request()  # 發送網路請求
         if status_code_or_message == 200:
             logger.info(f"{self.url} ─ {status_code_or_message}")
         else:
             logger.warning(f"{self.url} ─ {status_code_or_message}")
 
     async def close(self):
-        await self.session.close()
+        await self.session.close()  # 關閉會話
