9c444ee5c51caf6a5caf3ed65d29d2610316250b
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-17 10:23:26 +0800

    Add files via upload

---
+++ test/test_new_rss.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import asyncio
+
+from new_rss import start_API_Twitter
+
+
+class TestStartAPITwitter(unittest.TestCase):
+
+    async def mock_get_response_content(self):
+        return "mocked_response_content"
+
+    @patch('http_utility.HttpRequester.get_response_content', new_callable=MagicMock)
+    async def test_happy_path(self, mock_get_response_content):
+        mock_get_response_content.return_value = asyncio.Future()
+        mock_get_response_content.return_value.set_result(
+            "mocked_response_content")
+
+        url = "http://127.0.0.0.1"
+        twitter_api = start_API_Twitter(url)
+        response = await twitter_api.get_response()
+
+        self.assertEqual(response, ("mocked_response_content", {}))
+
+    @patch('http_utility.HttpRequester.get_response_content', new_callable=MagicMock)
+    async def test_invalid_url(self, mock_get_response_content):
+        mock_get_response_content.return_value = asyncio.Future()
+        mock_get_response_content.return_value.set_exception(ValueError)
+
+        url = "invalid_url"
+        twitter_api = start_API_Twitter(url)
+        response = await twitter_api.get_response()
+
+        self.assertIsNone(response)
+
+
+if __name__ == '__main__':
+    asyncio.run(unittest.main())
