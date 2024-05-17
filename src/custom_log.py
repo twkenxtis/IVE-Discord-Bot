540c1d57dead174a3f28b0eac923bdc3e6ada72d
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-17 10:57:36 +0800

    Update custom_log.py

--- src/custom_log.py
+++ src/custom_log.py
@@ -6,5 +6,5 @@ from loguru import logger
 logger_API_Twitter = logger.opt(depth=1, lazy=True)
 logger_API_Twitter_path = os.path.join(
     os.path.dirname(__file__), '..', 'assets', 'logs', 'twitter')
-logger_API_Twitter.add(f'{logger_API_Twitter_path}/'"{time}.log", level="WARNING",
-                       enqueue=True, rotation="20 MB")
+logger_API_Twitter.add(f'{logger_API_Twitter_path}/'"{time}.log",
+                       level="WARNING", encoding="utf-8", enqueue=True, rotation="20 MB")
