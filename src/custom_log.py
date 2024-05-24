defa643ab024b3839c2396ed572393d5eb4e4f7f
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2024-05-24 23:37:01 +0800

    Update custom_log.py

--- src/custom_log.py
+++ src/custom_log.py
@@ -14,3 +14,15 @@ logger_API_Twitter_path = os.path.join(
     os.path.dirname(__file__), '..', 'assets', 'logs', 'twitter')
 logger_API_Twitter.add(f'{logger_API_Twitter_path}/'"{time}.log",
                        level="WARNING", encoding="utf-8", enqueue=True, rotation="20 MB")
+
+logger_API__Discord = logger.opt(depth=1, lazy=True)
+logger_API__Discord_path = os.path.join(
+    os.path.dirname(__file__), '..', 'assets', 'logs', 'discord')
+logger_API__Discord.add(f'{logger_API__Discord_path}/'"{time}.log",
+                        level="WARNING", encoding="utf-8", enqueue=True, rotation="20 MB")
+
+logger_API__Main = logger.opt(depth=1, lazy=True)
+logger_API__Main_path = os.path.join(
+    os.path.dirname(__file__), '..', 'assets', 'logs')
+logger_API__Main.add(f'{logger_API__Main_path}/'"{time}.log",
+                     level="WARNING", encoding="utf-8", enqueue=True, rotation="20 MB")
